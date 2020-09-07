1037241461f7c889e364221739505bb321af75f5
felipelbhering <felipelbhering@gmail.com>
2020-09-07 20:18:32 -0300

    organizando scripts

--- R/main.R
+++ R/main.R
@@ -1,8 +1,14 @@
+#carregando bibliotecas
 library(magrittr)
 library(dplyr)
+
+#carregando dados
 imdb <- readr::read_rds("./data/imdb.rds")
 
+#carregando funcoes criadas
 source("./R/funcoes.R")
+
+#utilizando funcoes
 pegar_maiores_receitas(imdb, 10)
-calcular_num_filmes( tab = imdb,nome_ator = "Will Smith")
+calcular_num_filmes(tab = imdb, nome_ator = "Will Smith")
 
